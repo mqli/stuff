adb70085077b352900e17424401a4d9020003a26
mqli <limanqing315@gmail.com>
2013-02-06 17:11:36 +0800

    update

--- tree.py
+++ tree.py
@@ -1,13 +1,8 @@
-class Node:
-  def __init__(self, value):
-    self.value = value
-    self.childNodes = []
-  def append(self, node):
-    self.childNodes.append(node)
-  def visit(self, fn):
-    for node in self.childNodes:
-      node.visit(fn)
-    fn(self.value)
-tree = Node(1)
-tree.append(Node(2))
-tree.visit(print)
+def make_counter(x):
+  print('entering make_counter')
+  yield x                    
+  print('incrementing x')
+  x = x + 1
+counter = make_counter(2) 
+import itertools
+print 
