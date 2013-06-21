1cc3d0766c9c209df0ead723c4e975ea82403440
mqli <limanqing315@gmail.com>
2013-06-21 13:00:18 +0800

    ''

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-var net = require('net');
-var server = net.Server({});
-server.listen(9999,  function () {
-  console.log(9999);
-}).on('connection', function (socket) {
-  socket.setEncoding('utf8');
-  socket.write('hello');
-  socket.on('data', function (buffer) {
-    console.log(buffer);
-  });
-});
+var http = require('http'),
+    fs = require('fs');
+http.createServer(function(req,res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(fs.readFileSync('blocks.html'));
+}).listen(80);
